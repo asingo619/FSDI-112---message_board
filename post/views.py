b0a80c66be26b6eb09bf056d858925b5f55411fd
asingo619 <andrew.singo@gmail.com>
2021-11-04 21:53:04 -0700

    first commit

---
+++ post/views.py
@@ -0,0 +1,20 @@
+# from django.shortcuts import render
+from django.views.generic import (
+    TemplateView, 
+    ListView, 
+    DetailView
+)
+from .models import Post
+
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "posts_list.html"
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
